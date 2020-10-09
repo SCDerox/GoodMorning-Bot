6d93196464af3c62a12cf0d170da140b8c530a4a
Simon <mail@scderox.de>
2020-10-09 09:55:24 +0000

    ✨ Added data dir to setup

--- setup.js
+++ setup.js
@@ -3,28 +3,29 @@ const fs = require("fs");
 console.log("Seting up GoodMorningBot...")
 
 checkDir("config")
+checkDir("data", true)
 
-fs.readdir('./plugins', (err, files) => {
+fs.readdir('./plugins', (err, files) => { // Folders in Plugin folders does not include files insteand is has other dirs with more ".example" files
     if (err) console.error(err);
     files.forEach(f => {
         checkDir("plugins/" + f)
     })
 });
 
-function checkDir(dir) {
+function checkDir(dir, skip_warning = false) { // Check every file in a dictionary for ".example" files 
     fs.readdir(dir, (err, files) => {
         if (err) console.error(err);
         files.forEach(f => {
-            renameFile(dir + "/" + f)
+            renameFile(dir + "/" + f, skip_warning)
         })
     });
 }
 
-function renameFile(f) {
+function renameFile(f, skip_warning = false) { // Removing ".example" in file names  
     if (f.toString().includes("example")) {
         fs.rename(f, f.replace(".example", ""), function (err) {
             if (err) console.log('ERROR: ' + err);
-            console.log("[NEED MANUEL EDIT]: " + f.replace(".example", ""))
+            if(!skip_warning) console.log("[NEED MANUAL EDIT]: " + f.replace(".example", ""))
         });
     }
 }
